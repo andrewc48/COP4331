a8458fa5ed40d528de4fd38eb5a5b8a5162dc6d4
biegehydra <connorhallman29@gmail.com>
2025-06-02 10:48:42 -0400

    CORS

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,53 +1,59 @@
 <?php
-        $inData = getRequestInfo();
+	header("Access-Control-Allow-Origin: http://localhost:5500");
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+		http_response_code(200);
+		exit();
+	}
+    $inData = getRequestInfo();
 
-        $firstName = $inData["firstName"];
-        $lastName = $inData["lastName"];
-        $phone = $inData["phone"];
-        $email = $inData["email"];
-        $userId = $inData["userId"];
+    $firstName = $inData["firstName"];
+    $lastName = $inData["lastName"];
+    $phone = $inData["phone"];
+    $email = $inData["email"];
+    $userId = $inData["userId"];
 
-        $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-        if ($conn->connect_error)
-        {
-                returnWithError( $conn->connect_error );
-        }
+    $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+    if ($conn->connect_error)
+    {
+            returnWithError( $conn->connect_error );
+    }
 
-        else
-        {
-                $stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName=? AND LastName=? AND Phone=? AND Email=? AND UserID=?");
-                $stmt->bind_param("sssss", $firstName, $lastName,$phone,$email,$userID);
-                $stmt->execute();
+    else
+    {
+            $stmt = $conn->prepare("DELETE FROM Contacts WHERE FirstName=? AND LastName=? AND Phone=? AND Email=? AND UserID=?");
+            $stmt->bind_param("sssss", $firstName, $lastName,$phone,$email,$userID);
+            $stmt->execute();
 
-                if($stmt->affected_rows > 0)
-                {
-                        returnWithError("");
-                }
+            if($stmt->affected_rows > 0)
+            {
+                    returnWithError("");
+            }
 
-                else
-                {
-                        returnWithError("Contact Not Found");
-                }
+            else
+            {
+                    returnWithError("Contact Not Found");
+            }
 
-                $stmt->close();
-                $conn->close();
-        }
+            $stmt->close();
+            $conn->close();
+    }
 
-        function getRequestInfo()
-        {
-                return json_decode(file_get_contents('php://input'), true);
-        }
+    function getRequestInfo()
+    {
+            return json_decode(file_get_contents('php://input'), true);
+    }
 
-        function sendResultInfoAsJson( $obj )
-        {
-                header('Content-type: application/json');
-                echo $obj;
-        }
+    function sendResultInfoAsJson( $obj )
+    {
+            header('Content-type: application/json');
+            echo $obj;
+    }
 
-        function returnWithError( $err )
-        {
-                $retValue = '{"error":"' . $err . '"}';
-                sendResultInfoAsJson( $retValue );
-        }
-
+    function returnWithError( $err )
+    {
+            $retValue = '{"error":"' . $err . '"}';
+            sendResultInfoAsJson( $retValue );
+    }
 ?>
--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -1,5 +1,11 @@
 <?php
-
+	header("Access-Control-Allow-Origin: http://localhost:5500");
+	header("Access-Control-Allow-Headers: Content-Type");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+	if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+		http_response_code(200);
+		exit();
+	}
 	$inData = getRequestInfo();
 	
 	$searchResults = "";
