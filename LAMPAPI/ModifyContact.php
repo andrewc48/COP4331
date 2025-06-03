53ad49544bda71fbc857cd77c79af4be4697ebac
biegehydra <connorhallman29@gmail.com>
2025-06-03 12:54:34 -0400

    Fix update

--- LAMPAPI/ModifyContact.php
+++ LAMPAPI/ModifyContact.php
@@ -18,33 +18,33 @@
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
 	if ($conn->connect_error)
 	{
-			returnWithError( $conn->connect_error );
+		returnWithError( $conn->connect_error );
 	}
 
 	else
 	{
-			$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ? , LastName = ?, Phone = ?,Email = ?, ID = ?, UserID = ?");
-			$stmt->bind_param("ssssii", $firstName, $lastName,$phone,$email,$contactID, $userId);
-			$stmt->execute();
-			$stmt->close();
-			$conn->close();
-			returnWithError("");
+		$stmt = $conn->prepare("UPDATE Contacts SET FirstName = ? , LastName = ?, Phone = ?,Email = ?, UserID = ? WHERE ID = ?");
+		$stmt->bind_param("ssssii", $firstName, $lastName,$phone,$email,$userId, $contactId);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
 	}
 
 	function getRequestInfo()
 	{
-			return json_decode(file_get_contents('php://input'), true);
+		return json_decode(file_get_contents('php://input'), true);
 	}
 
 	function sendResultInfoAsJson( $obj )
 	{
-			header('Content-type: application/json');
-			echo $obj;
+		header('Content-type: application/json');
+		echo $obj;
 	}
 
 	function returnWithError( $err )
 	{
-			$retValue = '{"error":"' . $err . '"}';
-			sendResultInfoAsJson( $retValue );
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
 	}
 ?>
